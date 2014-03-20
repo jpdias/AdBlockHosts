a2cc7248398ea987358dc7a4b9d945e54c98a551
JP <jpdias@live.com.pt>
2014-03-20 01:05:20 +0000

    Initial commit

---
+++ AdHostPatch/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace AdHostPatch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(83, 67);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(139, 42);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Block it!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 131);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(295, 23);
+            this.progressBar1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(235, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Please run this program always as Administrator. ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(80, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(145, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "No updates available.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(319, 166);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "AdBlock Hosts File Patcher";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ AdHostPatch/Form1.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdHostPatch
+{
+    public partial class Form1 : Form
+    {
+        static String[] sites =
+            {
+                "http://hosts-file.net/ad_servers.asp",
+                "http://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts&showintro=0&mimetype=plaintext",
+                "http://winhelp2002.mvps.org/hosts.txt",
+                "http://someonewhocares.org/hosts/hosts"
+            };
+        static string hostslocal = Path.Combine(Environment.SystemDirectory, @"drivers\etc\hosts");
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            Task.Factory.StartNew(() =>
+            {
+                var filedit = getFileEditTime(hostslocal);
+                Task<DateTime?>[] tasks = sites.Select(getPageEdittime).ToArray();
+
+                this.BeginInvoke(new MethodInvoker(delegate
+                {
+                    if (Task.WaitAll(tasks, 20000))
+                    {
+                        var numberofupdates = tasks.Count(site =>
+                        {
+                            if (site.Result.HasValue)
+                                return filedit < site.Result.Value;
+                            else
+                            {
+                                label2.Text = "No internet connection :(";
+                                return false;
+                            }
+                        });
+
+                        if (tasks.Any(t => t.Result.HasValue))
+                        {
+                            label2.Text = string.Format("Available updates {0}/{1}", numberofupdates, sites.Length);
+                            button1.Enabled = true;
+                        }
+                    }
+                    else
+                    {
+                        label2.Text = "No internet connection :(";
+                    }
+                }));
+            });
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var oldValue = button1.Text;
+            button1.Enabled = false;
+            button1.Text = "Running";
+
+            progressBar1.Style = ProgressBarStyle.Continuous;
+            progressBar1.Maximum = 100;
+            progressBar1.Value = 1;
+
+            var sitesTask = sites.Select(download).ToList();
+
+            using (StreamWriter w = File.CreateText("hosts_temp"))
+            {
+                foreach (var site in sitesTask)
+                {
+                    Log(site.Result, w);
+                    progressBar1.Value += 80/sites.Length;
+                }
+                //Log(adaway, w);
+              
+            }
+
+            progressBar1.Value = 85;
+            RemDuplicate();
+            progressBar1.Value = 95;
+           
+            File.Copy("hosts",  hostslocal, true);
+            progressBar1.Value = 100;
+            File.Delete("hosts");
+            File.Delete("hosts_temp");
+            button1.Text = oldValue;
+            button1.Enabled = true;
+        }
+        public static void Log(string logMessage, TextWriter w)
+        {
+            if (logMessage != null)
+            {
+                 w.WriteLine(logMessage);
+            }
+           
+        }
+        public static void RemDuplicate()
+        {
+            var sr = new StreamReader(File.OpenRead("hosts_temp"));
+            var sw = new StreamWriter(File.OpenWrite("hosts"));
+            
+            var lines = new HashSet<int>();
+            while (!sr.EndOfStream)
+            {
+                string line = sr.ReadLine();
+                if (!string.IsNullOrWhiteSpace(line) && line[0] != '#')
+                {
+                    
+                    var arr = line.Split(' ', '\t');
+                    if (arr.Length < 2)
+                        continue;
+                    int hc = arr[1].GetHashCode();
+                    if (lines.Contains(hc))
+                        continue;
+
+                    lines.Add(hc);
+                    sw.WriteLine(line);
+                }
+            }
+            sw.Flush();
+            sw.Close();
+            sr.Close();
+        }
+
+        public static Task<String> download(string URL)
+        {
+            var webClient = new WebClient();
+            return  Task<String>.Factory.StartNew(() =>
+            {
+                try
+                {
+                   return webClient.DownloadString(URL);
+                }
+                catch (Exception ex)
+                {
+                    Console.Write(ex);
+                    return null;
+                }
+            });
+        }
+
+        public static DateTime getFileEditTime(string filePath)
+        {
+            return new FileInfo(filePath).LastWriteTime;
+        }
+
+        public static Task<DateTime?> getPageEdittime(string url)
+        {
+            return Task<DateTime?>.Factory.StartNew(() =>
+            {
+                try
+                {
+                    var req = (HttpWebRequest) WebRequest.Create(url);
+                    var res = (HttpWebResponse) req.GetResponse();
+
+                    return res.LastModified;
+                }
+                catch (WebException ex)
+                {
+                    if (ex.Status == WebExceptionStatus.NameResolutionFailure)
+                        return null;
+                    return DateTime.Now;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                    return DateTime.Now;
+                }
+            });
+        }
+
+
+    }
+}
